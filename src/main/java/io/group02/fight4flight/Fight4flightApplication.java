f847982b7498a871a44814ef5c163b504e1581b9
fisayoadabs <fisayoadabs@gmail.com>
2023-11-25 18:25:36 -0700

    first commit

---
+++ src/main/java/io/group02/fight4flight/Fight4flightApplication.java
@@ -0,0 +1,15 @@
+package io.group02.fight4flight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+
+@SpringBootApplication
+@RestCountroller
+public class Fight4flightApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fight4flightApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/group02/fight4flight/Fight4flightApplicationTests.java
@@ -0,0 +1,13 @@
+package io.group02.fight4flight;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Fight4flightApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
