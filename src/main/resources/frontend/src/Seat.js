af84d55b254e55facf747fbe89192441ee5b07c5
T-o-mas <tomaskmet23@gmail.com>
2023-12-02 04:11:38 -0700

    added flight browsing

--- src/main/resources/frontend/src/Seat.js
+++ src/main/resources/frontend/src/Seat.js
@@ -3,89 +3,142 @@ import { useNavigate } from "react-router-dom";
 import ButtonArray from "./ButtonArray"; // Import the ButtonArray component
 
 function Seat() {
+    const [seats, setSeats] = useState([]);
+
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await fetch('http://localhost:8080/seat/getAll');
+
+                if (!response.ok) {
+                    throw new Error(`HTTP error! Status: ${response.status}`);
+                }
+
+                const data = await response.json();
+                setSeats(data);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []); // Run once on component mount
+    console.log(seats);
     const navigate = useNavigate();
-    const LOCAL_STORAGE_KEY = "notesApp.notes";
-
-    const [notes, setNotes] = useState(() => {
-        const stored = JSON.parse(localStorage.getItem(LOCAL_STORAGE_KEY));
-        return stored || [];
-    });
-
-    const [seatData, setSeatData] = useState([]); // State for storing seat data from API
 
     const [showBusinessArray, setShowBusinessArray] = useState(false);
     const [showComfortArray, setShowComfortArray] = useState(false);
     const [showOrdinaryArray, setShowOrdinaryArray] = useState(false);
-    const [showFirstArray, setShowFirstArray] = useState(true);
+
     const [seatNumber, setSeatNumber] = useState(""); // State to hold the seat number
-    const [selectedSeat, setSelectedSeat] = useState("");
+    const [selectedButton, setSelectedButton] = useState(null);
 
     const toggleArray = (arrayType) => {
-        setShowBusinessArray(arrayType === "business");
-        setShowComfortArray(arrayType === "comfort");
-        setShowOrdinaryArray(arrayType === "ordinary");
-        setShowFirstArray(arrayType === "first"); // Add this line
+        setSelectedButton(arrayType);
+
+        switch (arrayType) {
+            case "business":
+                setShowBusinessArray(true);
+                setShowComfortArray(false);
+                setShowOrdinaryArray(false);
+                break;
+            case "comfort":
+                setShowComfortArray(true);
+                setShowBusinessArray(false);
+                setShowOrdinaryArray(false);
+                break;
+            case "ordinary":
+                setShowOrdinaryArray(true);
+                setShowBusinessArray(false);
+                setShowComfortArray(false);
+                break;
+            default:
+                break;
+        }
     };
 
-
-    const logButtonClick = (seatName) => {
-        setSeatNumber(seatName);
-        setSelectedSeat(seatName);
+    const logButtonClick = (label, row, col) => {
+        const newSeatNumber = `${String.fromCharCode("A".charCodeAt(0) + col)}${row}`;
+        setSeatNumber(newSeatNumber);
     };
-
 
     const clearSeatNumber = () => {
+        // Remove "clicked" class from all buttons
+        const buttons = document.querySelectorAll(".seat-button");
+        buttons.forEach((button) => button.classList.remove("clicked"));
         setSeatNumber("");
     };
-
-    useEffect(() => {
-        localStorage.setItem(LOCAL_STORAGE_KEY, JSON.stringify(notes));
-    }, [notes]);
-
-    useEffect(() => {
-        // Fetch seat data from the API
-        fetch("http://localhost:8080/seat/getAll") // Replace with your API URL
-            .then(response => response.json())
-            .then(data => setSeatData(data))
-            .catch(error => console.error("Error fetching seat data:", error));
-    }, []);
 
     return (
         <>
             <div id="title">
                 <h1>Seat Selection</h1>
             </div>
-            <div className="container">
-                <div className="left-menu">
+            <div class="container">
+                <div class="left-menu">
                     <div id="head">
                         <h2>Seat Type</h2>
                     </div>
-                    <button className="nav-menu-items" onClick={() => toggleArray("business")}>
+                    <button
+                        className={`nav-menu-items ${selectedButton === "business" ? "selected" : ""}`}
+                        onClick={() => toggleArray("business")}>
                         <h2>Business</h2>
                     </button>
-                    <button className="nav-menu-items" onClick={() => toggleArray("comfort")}>
+                    <button
+                        className={`nav-menu-items ${selectedButton === "comfort" ? "selected" : ""}`}
+                        onClick={() => toggleArray("comfort")}>
                         <h2>Comfort</h2>
                     </button>
-                    <button className="nav-menu-items" onClick={() => toggleArray("ordinary")}>
+                    <button
+                        className={`nav-menu-items ${selectedButton === "ordinary" ? "selected" : ""}`}
+                        onClick={() => toggleArray("ordinary")}>
                         <h2>Ordinary</h2>
                     </button>
-                    <br />
+                    <br>
+                    </br>
+                    <br></br>
                     <div id="head">
                         <h2>Seat: {seatNumber}</h2>
-                        <button onClick={clearSeatNumber} className="remove">
+                        <button onClick={clearSeatNumber} class="remove">
                             <b>Remove</b>
                         </button>
                     </div>
-                    <button className="continue" onClick={() => navigate("guest/insurance")}>
+                    <button class="continue"
+                            onClick={() => {
+                                if (seatNumber) {
+                                    navigate("/guest/insurance");
+                                } else {
+                                    alert("Please select a seat before continuing.");
+                                }
+                            }}>
                         <h2>Continue</h2>
                     </button>
                 </div>
-                <div className="right-content">
-                    {showFirstArray && (
+                <div class="right-content">
+                    {showBusinessArray && (
                         <ButtonArray
-                            seatData={seatData.filter(seat => seat.seattype === "First")}
-                            onButtonClick={logButtonClick}
-                            selectedSeat={selectedSeat}
+                            onButtonClick={(row, col) => logButtonClick('', row, col)}
+                            rows={5}
+                            cols={6}
+                            offset={0}
+                        />
+                    )}
+
+                    {showComfortArray && (
+                        <ButtonArray
+                            onButtonClick={(row, col) => logButtonClick('', row, col)}
+                            rows={6}
+                            cols={6}
+                            offset={5}
+                        />
+                    )}
+
+                    {showOrdinaryArray && (
+                        <ButtonArray
+                            onButtonClick={(row, col) => logButtonClick('', row, col)}
+                            rows={9}
+                            cols={6}
+                            offset={11}
                         />
                     )}
                 </div>
